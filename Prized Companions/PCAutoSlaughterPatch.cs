a2ab1fac5877bb237dc1130bdbd79294070845ee
ZenthWolf <ZenthRelin@gmail.com>
2021-12-28 08:46:05 -0500

    Add project files.

---
+++ Prized Companions/PCAutoSlaughterPatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using Verse;
+
+namespace Prized_Companions
+{
+    [HarmonyPatch(typeof(AutoSlaughterManager), "CanEverAutoSlaughter")]
+    internal static class PrizedCompanionsCantBeSlaughteredPatch
+    {
+        private static void Postfix(ref bool __result, Pawn animal)
+        {
+            if (__result)
+            {
+                __result = (animal.Name.Numerical);
+            }
+        }
+    }
+}
---
+++ Prized Companions/PCNotifyNames.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+using Verse;
+
+namespace Prized_Companions
+{
+    [HarmonyPatch(typeof(Dialog_NamePawn), "DoWindowContents")]
+    internal static class PrizedCompanionsNamedNotification
+    {
+        private static void Postfix(Pawn ___pawn)
+        {
+            ___pawn.Map.autoSlaughterManager.Notify_PawnSpawned();
+
+            //Log.Message("[Prized Companions] New Name clears list");
+        }
+    }
+}
