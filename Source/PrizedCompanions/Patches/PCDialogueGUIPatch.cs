50e49877ad20342ad656cef6b230581849504a30
ZenthWolf <ZenthRelin@gmail.com>
2022-02-04 00:46:25 -0500

    Alternate method branch start

--- Source/PrizedCompanions/Patches/PCDialogueGUIPatch.cs
+++ Source/PrizedCompanions/Patches/PCDialogueGUIPatch.cs
@@ -20,6 +20,7 @@ namespace Prized_Companions
         private static void Postfix(ref float __result)
         {
             __result -= 68;
+            //By default, returns labelWidth back to 62 px
         }
     }
 
@@ -82,146 +83,17 @@ namespace Prized_Companions
             rect.y -= 24f;
             rect.height += 25;
             offset = 932 + LabelWidth;
+            rect.x += rect.width - 60;
             GUI.BeginGroup(rect);
             WidgetRow widgetRow = new WidgetRow(0.0f, 0.0f);
             TextAnchor anchor2 = Text.Anchor;
             Text.Anchor = TextAnchor.MiddleCenter;
-            widgetRow.Label(string.Empty, offset);
             widgetRow.Label((string)"\nYoungest\nFirst\n", 60, (string)"Look. I never said prizing animals as companions meant NOT killing the young children...".Translate(), 48f);
             Text.Anchor = anchor2;
-            GUI.color = Color.gray;
-            Widgets.DrawLineVertical(offset, 0.0f, (float)((double)rect.height + 1.0));
-            GUI.color = Color.white;
             GUI.EndGroup();
         }
     }
 
-    /* BE FEARFUL
-        [HarmonyPatch(typeof(Dialog_AutoSlaughter), "DoAnimalHeader")]
-        internal static class PCDialogueHeaderPatch
-        {
-            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
-            {
-                // New Locals
-                var storedX = generator.DeclareLocal(typeof(float));
-                var storedY = generator.DeclareLocal(typeof(float));
-                List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
-
-                bool doFirst = false;
-                bool doBonusDup = true;
-                bool doSecond = true;
-                bool done = false;
-
-                for (int i = 0; i < codes.Count(); ++i)
-                {
-                    if (done)
-                    {
-                        yield return codes[i];
-                        continue;
-                    }
-                    else if (doFirst)
-                    {
-                        if (codes[i].opcode == OpCodes.Call && codes[i - 5].operand?.ToString() == "AnimalBonded")
-                        {
-                            yield return codes[i];
-
-                            //AddCurrentAndMaxEntries
-                            //Sets the upper label 
-
-                            yield return new CodeInstruction(OpCodes.Ldarg_0);
-                            //This should be a Keyed string for translation
-                            yield return new CodeInstruction(OpCodes.Ldstr, "Reverse");
-                            //Shifts labelbox width (Text is centered, min is -60)
-                            yield return new CodeInstruction(OpCodes.Ldc_R4, -40.0f);
-                            //Shifts labelbox width (Text is centered, min is -56)
-                            //I don't understand- this is associated with an empty string, but still affects placement of the text
-                            yield return new CodeInstruction(OpCodes.Ldc_R4, 16f);
-                            yield return new CodeInstruction(OpCodes.Ldloca_S, 0);
-                            yield return new CodeInstruction(OpCodes.Ldloca_S, 5);
-                            yield return codes[i].Clone();
-
-                            doFirst = false;
-                            continue;
-                        }
-                        else
-                        {
-                            yield return codes[i];
-                            continue;
-                        }
-                    }
-                    else if (doBonusDup)
-                    {
-                        if (codes[i].opcode == OpCodes.Pop && codes[i - 5].operand?.ToString() == "AutoSlaughterHeaderTooltipCurrentBonded")
-                        {
-                            yield return codes[i];
-                            yield return new CodeInstruction(OpCodes.Dup);
-
-                            doBonusDup = false;
-                            continue;
-                        }
-                        else
-                        {
-                            yield return codes[i];
-                            continue;
-                        }
-
-                    }
-                    else if(doSecond)
-                    {
-                        if (codes[i].opcode == OpCodes.Pop && codes[i - 5].operand?.ToString() == "AutoSlaughterHeaderTooltipAllowSlaughterBonded")
-                        {
-                            yield return codes[i];
-                            //Disregard data encapsulation, what could go wrong?
-                            yield return new CodeInstruction(OpCodes.Dup); 
-                            yield return new CodeInstruction(OpCodes.Callvirt, typeof(WidgetRow).GetProperty("FinalX").GetGetMethod());
-                            yield return new CodeInstruction(OpCodes.Stloc, storedX);
-                            //yield return new CodeInstruction(OpCodes.Dup);
-                            yield return new CodeInstruction(OpCodes.Callvirt, typeof(WidgetRow).GetProperty("FinalY").GetGetMethod());
-
-                            yield return new CodeInstruction(OpCodes.Ldc_R4, 24f);
-                            yield return new CodeInstruction(OpCodes.Sub);
-                            yield return new CodeInstruction(OpCodes.Stloc, storedY);
-                            yield return new CodeInstruction(OpCodes.Ldloc, storedX);
-                            yield return new CodeInstruction(OpCodes.Ldloc, storedY);
-                            yield return new CodeInstruction(OpCodes.Ldc_I4_2);
-                            yield return new CodeInstruction(OpCodes.Ldc_R4, 99999f);
-                            yield return new CodeInstruction(OpCodes.Ldc_R4, 4f);
-                            yield return new CodeInstruction(OpCodes.Newobj, typeof(WidgetRow).GetConstructor(new Type[] { typeof(float), typeof(float), typeof(Verse.UIDirection), typeof(float), typeof(float) }));
-
-                            //widgetRow.Label((str) text, width, (str) TTip, height)
-                            yield return new CodeInstruction(OpCodes.Ldstr, "Transverse\nCull");
-                            yield return new CodeInstruction(OpCodes.Ldc_R4, 92f);
-                            yield return new CodeInstruction(OpCodes.Ldstr, "TestTip");
-                            //yield return new CodeInstruction(OpCodes.Ldc_R4, -1.0f); //defaults height
-                            yield return new CodeInstruction(OpCodes.Ldc_R4, 48.0f); //double height
-                            yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(WidgetRow), nameof(WidgetRow.Label)));
-                            yield return new CodeInstruction(OpCodes.Pop);
-
-                            //yield return new CodeInstruction(OpCodes.Ldstr, "Test3");
-                            //yield return new CodeInstruction(OpCodes.Ldc_R4, 72f);
-                            //yield return new CodeInstruction(OpCodes.Ldstr, "TestTip2");
-                            //yield return new CodeInstruction(OpCodes.Ldc_R4, -1.0f);
-                            //yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(WidgetRow), nameof(WidgetRow.Label)));
-                            //yield return new CodeInstruction(OpCodes.Pop);
-
-                            doSecond = false;
-                            done = true;
-                            continue;
-                        }
-                        else
-                        {
-                            yield return codes[i];
-                            continue;
-                        }
-                    }
-                    else
-                    {
-                        yield return codes[i];
-                    }
-                }
-            }
-        }
-        */
     [HarmonyPatch(typeof(Dialog_AutoSlaughter), "DoWindowContents")]
     internal static class PCDrawDelimiter
     {
