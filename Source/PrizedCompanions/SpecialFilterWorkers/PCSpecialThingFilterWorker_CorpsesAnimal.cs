609c7f205abf7c4d7899cae403f9048edfbf8e2a
ZenthWolf <ZenthRelin@gmail.com>
2022-11-13 01:51:22 -0500

    Adds filter for Companion and NonCompanion animals.

---
+++ Source/PrizedCompanions/SpecialFilterWorkers/PCSpecialThingFilterWorker_CorpsesAnimal.cs
@@ -0,0 +1,9 @@
+﻿using Verse;
+
+namespace RimWorld
+{
+    public class PCSpecialThingFilterWorker_CorpsesAnimal : SpecialThingFilterWorker
+    {
+        public override bool Matches(Thing t) => t is Corpse corpse && corpse.InnerPawn.def.race.Animal && (corpse.InnerPawn.Name.Numerical || corpse.InnerPawn.Faction != Faction.OfPlayer);
+    }
+}
---
+++ Source/PrizedCompanions/SpecialFilterWorkers/PCSpecialThingFilterWorker_CorpsesCompanion.cs
@@ -0,0 +1,9 @@
+﻿using Verse;
+
+namespace RimWorld
+{
+    public class PCSpecialThingFilterWorker_CorpsesCompanion : SpecialThingFilterWorker
+    {
+        public override bool Matches(Thing t) => t is Corpse corpse && corpse.InnerPawn.def.race.Animal && corpse.InnerPawn.Faction == Faction.OfPlayer && !corpse.InnerPawn.Name.Numerical;
+    }
+}
